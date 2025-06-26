3ddeac8aa235bb7a8db165dcaaf3f13b58cbe55c
TUBVVTK-VTSIM12 <ana.oravitan@tum.de>
2025-06-26 17:04:16 +0200

    nav mesh runtime baking 0.1

--- Scripts/BusStationController.cs
+++ Scripts/BusStationController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.AI.Navigation;
+using UnityEngine.AI;
 
 
 namespace tum_car_controller
@@ -9,10 +11,16 @@ namespace tum_car_controller
     {
         // Fields
         private BoxCollider busStationCollider;
+        private NavMeshSurface busStationNavMeshSurface;
+        private NavMeshSurface busFloorNavMeshSurface;
+        private NavMeshSurface busFrontRampNavMeshSurface;
+        private NavMeshSurface busRearRampNavMeshSurface;
+
 
         // Flags
         private bool busInStation = false;
-        private bool arrived = false;
+        private bool startedBoarding = false;
+        private bool endedBoarding = false;
         private int frameCounter = 0;
 
         // Bus
@@ -37,35 +45,45 @@ namespace tum_car_controller
         }
 
         void FixedUpdate()
-        {   
+        {
             if (bus != null)
             {
                 if (busInStation)
                 {
                     busSpeed = bus.GetComponent<BusController>().currentSpeed;
-                    Debug.Log("Bus Speed: " + busSpeed);
                     DoorManager busDoorManager = bus.GetComponent<DoorManager>();
 
-                    if (busSpeed == 0)
-                        frameCounter++;
-                    else
-                        frameCounter = 0; // Reset if bus is moving
-                    Debug.Log("Frame Counter: " + frameCounter);
+                    if (!startedBoarding && !endedBoarding)
+                    {
+                        // We are checking if the bus is stationary only when it is in the station and has not started boarding yet
+                        if (busSpeed == 0)
+                            frameCounter++;
+                        else
+                            frameCounter = 0; // Reset if bus is moving
 
-                    if (busSpeed == 0 && arrived == false && frameCounter > 10)
-                    {
-                        arrived = true;
-                        busDoorManager.openDoorsBasedOnScene();
-                        Debug.Log("Bus has arrived at the station and doors are opening.");
+                        if (busSpeed == 0 && frameCounter > 10)
+                        {
+                            // Debug.Log("Bus has arrived at the station and doors are opening.");
+                            startedBoarding = true;
+                            busDoorManager.openDoorsBasedOnScene();
+                            Invoke(nameof(BakeNavMesh), 0.5f);
+                        }
                     }
-                    else if (busSpeed > 0 && arrived == true) // leaving the station
+                    else if (busSpeed > 0 && startedBoarding && !endedBoarding) // leaving the station
                     {
-                        arrived = false; // Reset for next arrival
+                        // Debug.Log("StartedBoarding: " + startedBoarding + ", EndedBoarding: " + endedBoarding);
+                        // Debug.Log("Bus is moving out of the station area. Closing doors.");
+                        endedBoarding = true;
                         busDoorManager.CloseDoorsBasedOnScene();
-                        Debug.Log("Bus is moving out of the station area.");
                     }
                 }
-
+                else if (startedBoarding && endedBoarding)
+                {
+                    startedBoarding = false;
+                    endedBoarding = false;
+                    frameCounter = 0; // Reset frame counter
+                    // Debug.Log("Resetting boarding state as bus has left the station.");
+                }
             }
         }
 
@@ -75,9 +93,9 @@ namespace tum_car_controller
 
             if (other.CompareTag(busWheelTag))
             {
+                Debug.Log("Bus has entered the station area.");
                 busInStation = true;
                 bus = other.transform.root.gameObject;
-                Debug.Log("Bus has entered the station area.");
             }
         }
 
@@ -92,5 +110,38 @@ namespace tum_car_controller
         }
 
         // Utility Methods
+
+        void BakeNavMesh()
+        {
+            // 1. Find the transforms of all relevant objects
+            Transform busStation = this.transform;
+            Transform busFloor = bus.transform.Find("BusFloor");
+            Transform frontRamp = bus.transform.Find("FrontRamp");
+            Transform rearRamp = bus.transform.Find("RearRamp");
+
+            // 2. Store original parents
+            Transform[] objects = { busStation, busFloor, frontRamp, rearRamp };
+            Transform[] originalParents = new Transform[objects.Length];
+            for (int i = 0; i < objects.Length; i++)
+                originalParents[i] = objects[i].parent;
+
+            // 3. Create temporary parent
+            GameObject tempParent = new GameObject("TempNavMeshParent");
+            foreach (var obj in objects)
+                obj.parent = tempParent.transform;
+
+            // 4. Add NavMeshSurface to temp parent and bake
+            var navMeshSurface = tempParent.AddComponent<NavMeshSurface>();
+            navMeshSurface.collectObjects = CollectObjects.Children;
+            navMeshSurface.BuildNavMesh();
+
+            // 5. Restore original parents
+            for (int i = 0; i < objects.Length; i++)
+                objects[i].parent = originalParents[i];
+
+            // 6. Destroy the temporary parent (NavMesh remains)
+            // Destroy(tempParent)
+        }
+
     }
 }
